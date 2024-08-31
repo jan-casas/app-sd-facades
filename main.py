7e3cbc92958d59dc8f06f2bed3f14f2d46584ed5
Jan <casasvil@protonmail.com>
2024-08-31 18:31:14 +0200

    Fix: Fix main files to deploy docker

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from core_callbacks import *
-from callbacks import (callback_modal, callback_response, callback_chat)
 import logging
 import sys
 
+from core_callbacks import *
+from callbacks import (callback_response, callback_chat)
+# from callbacks import (callback_modal, callback_response, callback_chat)
+
 # TODO:
+# Añadir matriz con la distribución (curve) de cada variable y la posición de la variable
+# seleccionada
 """
 Load section:
 - Bake urban data in postgres and read geojson from server not in local (query done)
@@ -14,11 +18,14 @@ Home section:
 
 Assets section:
 - Create the functionality to compare your current assets at scale
-[X]- Add graphs to show more valuable information -> CUrrent graph must display the analysis 
-types not the cities (it 
-filter when the table value is selected). Add graph about building age, building type, etc.
-- Export option to download the data
+[X]- Add graphs to show more valuable information -> Current graph must display the analysis 
+types not the cities (it filter when the table value is selected). Add graph about building age, 
+building type, etc.
+- Filter table data with the parallel graph
 
+
+
+---- OPTIONAL ----
 Explore preferences:
 - Add graphs to filter the data
 - Add a kind of "search copilot" functionality to filter the data by natural language
@@ -27,6 +34,5 @@ Explore preferences:
 logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)],
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8060)
+    app.run(debug=False, host='0.0.0.0', port=5000)
