b7e3e80df0159b9de9baaa5b0b41ae9cc4b728c5
luis <60604118+jan-casas@users.noreply.github.com>
2024-09-28 20:34:34 +0200

    feat: remove deprecated files and update structure
    - Deleted `config/settings.py` as it was no longer needed.
    - Removed font licenses and README files from `static/fonts/Open_Sans` and `static/fonts/Roboto_Mono`.
    - Deleted unused SVG icons in `static/icons`.
    - Cleaned up `static/style.css` to remove commented-out styles.
    - Removed unused utility functions in `utils/login_handler.py` and `utils/utils.py`.
    - Updated import paths in `main.py` and removed logging configuration.
    - Adjusted `.gitignore` to reflect the new directory structure.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-import sys
+from src.config.logs import *
+from src.core_callbacks import *
 
-from core_callbacks import *
-from callbacks import (callback_response, callback_chat)
 # from callbacks import (callback_modal, callback_response, callback_chat)
 
 # TODO:
@@ -17,7 +15,7 @@ Home section:
 - Bake the analytic example models in Speckle and link them to the app
 
 Assets section:
-- Create the functionality to compare your current assets at scale
+- Create the functionality to compare your current performance_assets at scale
 [X]- Add graphs to show more valuable information -> Current graph must display the analysis 
 types not the cities (it filter when the table value is selected). Add graph about building age, 
 building type, etc.
@@ -31,9 +29,6 @@ Explore preferences:
 - Add a kind of "search copilot" functionality to filter the data by natural language
 """
 
-logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)],
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, use_reloader=False)  # , host='0.0.0.0', port=5000)
     # FIXME: RUN ON CHROME
